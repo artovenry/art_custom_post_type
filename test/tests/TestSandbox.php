d998da9058520c03c53cd28ee741ce0170671b33
artovenry <artovenry@gmail.com>
2016-08-19 19:41:05 +0900

    commit

--- test/tests/TestSandbox.php
+++ test/tests/TestSandbox.php
@@ -3,4 +3,7 @@ class TestSandbox extends PHPUnit_Framework_TestCase{
   function test_wp(){
     $this->assertTrue(function_exists("wp_head"));
   }
+  function test_wp(){
+    $this->assertTrue(function_exists("has_custom_logo"));
+  }
 }
