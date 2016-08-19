7596f52e56763a9e59ff029928b9b9cbda1c73e5
artovenry <artovenry@gmail.com>
2016-08-19 19:26:29 +0900

    commit

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,4 +2,6 @@
 require dirname(__DIR__) . "/vendor/autoload.php";
 global $wp, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header,$current_user;
 define('WP_USE_THEMES', false);
-require __DIR__ . '/wp/3.8/wp-blog-header.php';
+
+$wp_version= getenv("WP_VERSION");
+require __DIR__ . "/wp/${wp_version}/wp-blog-header.php";
