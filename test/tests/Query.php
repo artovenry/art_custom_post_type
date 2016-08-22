2676e5ec26405043442813ae48f1f4af2507244a
vegeta <dev@vegeta.co.jp>
2016-08-22 12:12:43 +0900

    commit

---
+++ test/tests/Query.php
@@ -0,0 +1,8 @@
+<?
+class TestSandbox extends PHPUnit_Framework_TestCase{
+  function test_wp(){
+    $this->assertTrue(function_exists("wp_head"));
+    $this->assertTrue(function_exists("custom_theme_function"));
+    $this->assertTrue(class_exists("Artovenry\CustomPostType\Base"));
+  }
+}
