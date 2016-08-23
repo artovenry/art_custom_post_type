6f235a87f04955acce16bc1222486e834cc1e128
vegeta <dev@vegeta.co.jp>
2016-08-23 16:03:48 +0900

    commit

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?
 /* Plugin Name: art_custom_post_type */
 require __DIR__ . "/vendor/autoload.php";
-require __DIR__ . "/lib/constants.php";
-require __DIR__ . "/lib/functions.php";
 Artovenry\CustomPostType\Initializer::run();
