09ef94bfd62d85448dfbacf6528674ee72f0f98d
vegeta <dev@vegeta.co.jp>
2016-08-24 19:33:39 +0900

    add meta_attributename's  regexp restriction

--- lib/constants.php
+++ lib/constants.php
@@ -4,3 +4,4 @@ const PREFIX= "art_";
 const MODELS= "models";
 const META_BOXES= "views/meta_boxes";
 const DEFAULT_RENDERER= "Haml"; //Wp or Haml
+const META_ATTRIBUTE_NAME_REGEXP= "/\A[a-z][a-z0-9_]*\z/";
