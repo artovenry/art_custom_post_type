c15b25411fdbb5a5f2c0190a067a053518c891bd
artovenry <artovenry@gmail.com>
2016-09-15 15:05:18 +0900

    commit

--- test/theme/custom_post_types/TypeOne.php
+++
@@ -1,7 +0,0 @@
-<?
-class TypeOne extends Artovenry\CustomPostType\Base{
-  static $meta_attributes=["show_at_home", "scheduled_on","file_one_path", "file_two_path"];
-  static $meta_boxes=[
-    ["name"=>"uploader"]
-  ];
-}
