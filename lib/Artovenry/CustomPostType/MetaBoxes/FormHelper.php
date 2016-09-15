6d7543b1361d5b07290cb33bd7eaf875bf7e77b0
vegeta <dev@vegeta.co.jp>
2016-09-15 19:39:25 +0900

    commit

--- lib/Artovenry/CustomPostType/MetaBoxes/FormHelper.php
+++ lib/Artovenry/CustomPostType/MetaBoxes/FormHelper.php
@@ -2,6 +2,14 @@
 namespace Artovenry\CustomPostType\MetaBoxes;
 
 class FormHelper{
+
+  //alias for field_name_for
+  function name(){
+    return call_user_func_array([$this, "field_name_for"], func_get_args());
+  }
+  function field_name_for($name){
+    return \Artovenry\CustomPostType\PREFIX. "meta_boxes{$name}";
+  }
   function text_field_for($record, $attr){
     $tag= '<input type="text" name="%s" value="%s" />';
     $name= \Artovenry\CustomPostType\PREFIX. "meta_boxes[{$record->post_type}][{$attr}]";
